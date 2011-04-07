afa055d756696ecd07e5e5d1d5d7e6626d2a964f
Hugo Engström <hugo.engstrom@gmail.com>
2011-04-07 12:09:31 +0200

    testasadsdadadaadasdadasdasdsassadsadasdasdasdsaasdasdsadasda

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -9,7 +9,7 @@ namespace git_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("testasads");
+            Console.WriteLine("testasadsdadadaadasdadasdasd");
         }
     }
 }
